1705c39f184ced0ee271a949c06ac41d0fd01a5b
ugolino <dan.ugolini@gmail.com>
2017-05-16 16:10:26 +0200

    init

---
+++ share.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<html>
+<head>
+<title>Flappy Lacerba</title>
+<meta property="og:title" content="Impara a creare VideoGiochi!"/>
+<meta property="og:image" content="http://flappy.lacerba.io/share.jpg"/>
+<meta property="og:site_name" content="Lacerba.io"/>
+<meta property="og:description" content="Segui il corso gratuito su come creare un clone di Flappy Bird in poco più di 2 ore senza scrivere una linea di codice!"/>	
+</head>
+<body>
+<meta http-equiv="refresh" content="0;URL=http://flappy.lacerba.io/" />
+</body>
+</html>
